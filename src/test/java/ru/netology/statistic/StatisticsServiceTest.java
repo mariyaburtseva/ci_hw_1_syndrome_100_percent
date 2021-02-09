c91be55dedbe7728d03c856149494f77be45f977
Burtseva Mariya <mariyaburceva9898@mail.ru>
2021-02-09 17:15:29 +0300

    update statisticService and statisticServiceTest

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,8 +1,5 @@
-
 package ru.netology.statistic;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
@@ -10,12 +7,7 @@ class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
+        assertEquals(12, service.findMax(incomesInBillions));
     }
 }
