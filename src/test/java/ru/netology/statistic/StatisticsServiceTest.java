011f4ebd23fda2d3e197723b6bca57edab83b6b8
Burtseva Mariya <mariyaburceva9898@mail.ru>
2021-02-09 20:20:19 +0300

    Update StatisticsServiceTest

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,4 +10,11 @@ class StatisticsServiceTest {
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         assertEquals(12, service.findMax(incomesInBillions));
     }
+
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        assertEquals(12, service.findMax(incomesInBillions));
+    }
 }
